1850ac86e1ef064907dc3068e60ce609d180c6f5
Mathias Funk <m.funk@tue.nl>
2026-08-28 19:55:14 +0200

    v0.9.29

--- modules/common/app/utils/StringUtils.java
+++ modules/common/app/utils/StringUtils.java
@@ -10,7 +10,7 @@ import com.google.common.html.HtmlEscapers;
 
 public class StringUtils {
 
-	public static final String VERSION = "0.9.28";
+	public static final String VERSION = "0.9.29";
 
 	/**
 	 * null safe string
